183c7a3e168b5642d3b2235ae0ecb4fc195d5171
FezChat <frediezra001@gmail.com>
2025-11-28 10:57:32 +0300

    Delete src directory

--- src/integrations/supabase/client.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = "https://xhsnuyouuzwrktyisnhv.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhoc251eW91dXp3cmt0eWlzbmh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzMDAyNjIsImV4cCI6MjA3OTg3NjI2Mn0.vUuFmMKUS8H5fYqStvWv8lQN-mnRfvBb-uGQd7LAZuE";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
